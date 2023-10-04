648b4b0cdad73bf84f93a528d69fd389b97b8140
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-10-04 10:43:43 +0200

    cmt

---
+++ ejerciciopizas/DataBaseManager.py
@@ -0,0 +1,7 @@
+from ejerciciopizas.Authenticator import Authenticator
+
+
+class DataBaseManager:
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+        
